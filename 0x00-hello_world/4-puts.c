7193bf770c373489498810f9862aac16b63f5584
Joy Dzidula <rejoicesparkle8565@gmail.com>
2023-04-26 07:27:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 * Return: Always o (success)
 */
 int main(void)
-{	puts("\"Programming is like building a multilingual puzzle\n");
+{	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
