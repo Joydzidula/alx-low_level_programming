8a38c74dd2d2272b61fd7d906601ae18915f38e1
Joy Dzidula <rejoicesparkle8565@gmail.com>
2023-04-26 07:17:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* 
+*
 * Return: Always o (success)
 */
 int main(void)
-{	puts("Programming is like building a multilingual puzzle\n");
+{	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
